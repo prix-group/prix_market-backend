9a7f83e391ad6bc36eee515e0c6d08d78f242d42
Daniyar Otynshin <daniyar.otynshin@gmail.com>
2023-01-25 01:28:00 +0600

    feature: add exception handler for get post method

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,19 +1,35 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('posts')
 @ApiTags('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @ApiOperation({ summary: 'Get All Posts' })
+  @ApiResponse({ status: 200 })
   @Get()
   findAll() {
     return this.postsService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get Post By Id' })
+  @ApiResponse({ status: 200 })
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postsService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    const post = this.postsService.findOne(id);
+
+    if (!post) {
+      throw new NotFoundException(`Post with ${id} does not exist.`);
+    }
+
+    return post;
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,14 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { CreatePostDto } from './dto/create-post.dto';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class PostsService {
   constructor(private prisma: PrismaService) {}
-
-  create(createPostDto: CreatePostDto) {
-    return this.prisma.post.create({ data: createPostDto });
-  }
 
   findAll() {
     return this.prisma.post.findMany({ where: { published: true } });
