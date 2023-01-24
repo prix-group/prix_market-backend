bf5f6d7baab0024d0b585d025482f86eb0c615d5
Daniyar Otynshin <daniyar.otynshin@gmail.com>
2023-01-25 05:56:44 +0600

    config: use config in main.ts for port values

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PostsModule, PrismaModule],
+  imports: [PostsModule, PrismaModule, ConfigModule.forRoot()],
   controllers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,7 +18,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+
+  await app.listen(port);
 }
 
 bootstrap().then(() => console.log('server started'));
